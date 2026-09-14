102a33a9f3e4ed727532481029ef74b80b60238d
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2026-09-14 17:43:58 +0000

    Update crc64.cc

--- test/crc64.cc
+++ test/crc64.cc
@@ -14,7 +14,7 @@ test_getcrc64_byte_arrays()
 	assert(Digest::CRC::crc64("George", 6) == 6714394476893704192);
 	assert(Digest::CRC::crc64("Ringo", 5) == 6038837226071130112);
 	assert(Digest::CRC::crc64("", 0) == 0);
-	assert(Digest::XXHASH::xxh("\377\277\300\200", 4) == 15615382887346470912ULL);
+	assert(Digest::CRC::crc64("\377\277\300\200", 4) == 15615382887346470912ULL);
 	assert(Digest::XXHASH::xxh("John", 4) == 0x46034394ee588447ULL);
 	assert(Digest::XXHASH::xxh("Paul", 4) == 0xa3c9fd9dff86779aULL);
 	assert(Digest::XXHASH::xxh("George", 6) == 0x88290fd774650a6aULL);
