1ff435622426ebc51f7e7c2e00546365f1eecd31
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2026-09-14 16:30:35 +0000

    Update crc64.cc

--- lib/crc64.cc
+++ lib/crc64.cc
@@ -1,20 +1,13 @@
 #include "crucible/crc64.h"
-
 #include <xxhash.h>
-
-#include <iostream>
-#include <string>
-#include <vector>
-
-#define XXH_STATIC_LINKING_ONLY // Gives access to advanced algorithms like XXH3
 
 namespace crucible {
 
 	uint64_t
 	Digest::CRC::crc64(const void *p, size_t len)
 	{
-    uint64_t hash64 = XXH64(p, len, 0);
-		return hash64;
+		uint64_t xxhash = XXH64(p, len, 0);
+		return xxhash;
 	}
 
 
