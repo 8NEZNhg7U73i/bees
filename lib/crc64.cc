16c2c4c1524d6d3fd8c3c6bc4bbe4ae97dcb3284
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2026-09-14 16:14:47 +0000

    Update crc64.cc

--- lib/crc64.cc
+++ lib/crc64.cc
@@ -13,7 +13,7 @@ namespace crucible {
 	uint64_t
 	Digest::CRC::crc64(const void *p, size_t len)
 	{
-    XXH64_hash_t hash64 = XXH3_64bits_withSeed(p, len, 0);
+    XXH64_hash_t hash64 = XXH64(p, len, 0);
 		return hash64;
 	}
 
